fcafce96a067e3a3abe6c5bf1cdf2e02696526ed
BFORD1516 <151584741+BFORD1516@users.noreply.github.com>
2023-12-13 17:25:07 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+
+  // ... (rest of the fruit list)
+];
+
+function search(str) {
+  let results = [];
+
+  if (str.length > 0) {
+    const regex = new RegExp(str, "i");
+    results = fruit.filter((f) => regex.test(f));
+  }
+
+  return results;
+}
+
+function searchHandler(e) {
+  const inputVal = e.target.value;
+  const results = search(inputVal);
+  showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+  suggestions.innerHTML = "";
+
+  if (results.length > 0) {
+    results.forEach((result) => {
+      const li = document.createElement("li");
+      li.textContent = result;
+      suggestions.appendChild(li);
+    });
+    suggestions.style.display = "block";
+  } else {
+    suggestions.style.display = "none";
+  }
+}
+
+function useSuggestion(e) {
+  input.value = e.target.textContent;
+  suggestions.style.display = "none";
+}
+
+input.addEventListener("input", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
